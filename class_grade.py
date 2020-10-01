8b18b110bca7bc7db5a23899fb8d3f4601dd6ae6
J-TechTips <jtecktips@gmail.com>
2020-10-01 16:47:07 -0700

    Lab 3

---
+++ class_grade.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+"""" Plain English
+start
+create a list to store 5 number (float)
+capture user's input for their grades
+each time we capture the user's input, we append the number to the list
+sort the list ascending, then splice the items starrting at index 2
+once we have three highest number in the full list, we sum them up and divide by 3 oupt a message to the users
+end
+""""
+
+grade = {}
+
+grade = input  ("Enter the 1st graded")
+grades.appeds(float(grade)) 
+grade = input  ("Enter the 2nd graded")
+grades.appeds(float(grade))
+grade = input  ("Enter the 3rd graded")
+grades.appeds(float(grade))
+grade = input  ("Enter the 4th graded")
+grades.appeds(float(grade))
+grade = input  ("Enter the 5th graded")
+grades.appeds(float(grade))
+
+grades = sort{}
+grades = grade(2:)
+grades = sum(grades)
+result = grades /3
+
+print("Average Grade {0:21)%$".format(resuit))
